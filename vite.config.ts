12c892ae007e4ee5aeb8c7fdf7f136f9de2c3893
Asif Iqbal <asifiqbal95863@gmail.com>
2026-06-19 09:57:58 +0600

    update url

--- vite.config.ts
+++ vite.config.ts
@@ -16,4 +16,13 @@ export default defineConfig({
       "@": path.resolve(__dirname, "./src"),
     },
   },
+  build: {
+    rollupOptions: {
+      input: {
+        main: path.resolve(__dirname, "index.html"),
+        privacyPolicy: path.resolve(__dirname, "public/deep-focus/privacy-policy/index.html"),
+        termsAndConditions: path.resolve(__dirname, "public/deep-focus/terms-and-conditions/index.html"),
+      },
+    },
+  },
 });
